6dac20b2739f2075c31327bb602bb6fae532db65
James Halliday <mail@substack.net>
2013-08-01 01:31:13 -0700

    remove slice for more fastness

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-module.exports = function npair (xs, cb) {
-    if (xs.length === 1) {
+module.exports = function npair (xs, cb, ix) {
+    if (ix === undefined) ix = 0;
+    if (xs.length <= ix + 1) {
         for (var i = 0, l = xs[0].length; i < l; i++) cb([ xs[0][i] ]);
         return;
     }
-    var xs_ = xs.slice(0, -1);
-    npair(xs_, function (y) {
-        var z = xs[xs.length - 1];
+    npair(xs, function (y) {
+        var z = xs[xs.length - 1 - ix];
         for (var i = 0, l = z.length; i < l; i++) {
             cb(y.concat(z[i]));
         }
-    });
+    }, ix + 1);
 };
